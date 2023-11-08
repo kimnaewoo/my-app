583f1263ebad212b950c46edb6a934e652c95511
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-11-08 12:04:40 +0900

    23.11.08

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let isrc = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0QbO0Q6NCNVE4uxpooMpNkUrp0_pj_nnBWg&usqp=CAU'
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={isrc} className="App-logo" alt="logo" style={{borderRadius:'50%'}} />
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          슈퍼 쏜 슈퍼 쏜~
         </a>
       </header>
     </div>
